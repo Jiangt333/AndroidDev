2a5041e5b1c6089caa6c177aca57fd3be5c0a0fe
jt-dididadida <jt1170504445@163.com>
2023-05-29 17:03:18 +0800

    更新 实现了tab页面切换响应函数

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -8,6 +8,7 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.content.MutableContextWrapper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,11 +24,12 @@ public class LoginActivity extends AppCompatActivity {
 
     // 得到TabHost对象实例
     private TabHost tabhost;
-//    private TextView topbarText = findViewById(R.id.topbarText);
+    private TextView topbarText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         tabhost = (TabHost)findViewById(R.id.mytab);
         // 调用 TabHost.setup()
         tabhost.setup();
@@ -36,5 +38,28 @@ public class LoginActivity extends AppCompatActivity {
         tabhost.addTab(tabhost.newTabSpec("two").setIndicator("广场").setContent(R.id.tab2));
         tabhost.addTab(tabhost.newTabSpec("three").setIndicator("我的").setContent(R.id.tab3));
 
+        tabhost.setOnTabChangedListener(new TabHost.OnTabChangeListener(){
+            public void onTabChanged (String tabId) {
+                topbarText = findViewById(R.id.topbarText);
+                // onTabChanged是一个抽象类方法，控制tab页面切换响应，这里进行重写
+                int pos = tabhost.getCurrentTab();
+                if(pos == 0){
+                    topbarText.setText("首 页");
+                }
+                if(pos == 1){
+                    topbarText.setText("广 场");
+                }
+                if(pos == 2){
+                    topbarText.setText("我 的");
+                }
+            }
+
+        });
+
     }
+
+//    public void onClickTab(){
+//        topbarText = findViewById(R.id.topbarText);
+//
+//    }
 }
