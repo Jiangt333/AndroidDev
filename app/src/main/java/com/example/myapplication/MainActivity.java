5fe13f08620ec1c6aa188d363fd4f8b98709cfa2
weigp1 <2524634086@qq.com>
2023-06-08 23:47:21 +0800

    测试与后端服务响应

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,14 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import okhttp3.*;
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
 
@@ -25,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
     private static PreparedStatement stmt = null;
     private EditText Username;
     private EditText PassWord;
+    private Gson gson = new Gson();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,24 +57,28 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
                     .add("user",username)
                     .build();
             Request request = new Request.Builder()
-                    .url("http://172.27.115.196:8080/login")
+                    .url("http://172.17.19.156:8080/login")
                     .post(body)
                     .cacheControl(CacheControl.FORCE_NETWORK)
                     .build();
             client.newCall(request).enqueue(new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
                 }
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
                     if(response.isSuccessful()){//回调的方法执行在子线程。
-                        String RealPassword = response.body().string();
+                        String userJson= response.body().string();
+                        //Type type = new TypeToken<List<User>>() {
+                        //}.getType(); //泛型类型，import com.google.gson.reflect.TypeToken;
+                        User user = gson.fromJson(userJson, User.class); //反序列化
                         Intent intent = null;
-                        if (password.equals(RealPassword)) {
+                        if (password.equals(user.getRealpassword())) {
                             intent = new Intent(MainActivity.this, LoginActivity.class);
                             startActivity(intent);
                         } else {
-
+                            System.out.println("wrong");
                         }
                     }
                     else {
