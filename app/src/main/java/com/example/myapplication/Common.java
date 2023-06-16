b6124dc9f0326732c8c3a619e6894136153be48b
jt-dididadida <jt1170504445@163.com>
2023-06-16 11:51:28 +0800

    实现底部抽屉及删除按钮的部分响应（还未实现UI界面删除item）

--- app/src/main/java/com/example/myapplication/Common.java
+++ app/src/main/java/com/example/myapplication/Common.java
@@ -1,16 +1,32 @@
 package com.example.myapplication;
 
+import android.widget.ListView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Common {
 
-    public static String URL = "http://172.20.10.3:8080";
+    public static String URL = "http://172.17.22.225:8080";
 
+    public static List<listviewItem> lvItemList = new ArrayList<listviewItem>();
+    public static ListView listView;
+    public static mListAdapter adapter;
 
-    public static ArrayList<String> questionList;
-    public static ArrayList<String> answerList;
-    public static ArrayList<String> timeList;
+    public static ArrayList<Integer> idList = new ArrayList<>();
+    public static ArrayList<String> questionList = new ArrayList<>();
+    public static ArrayList<String> answerList = new ArrayList<>();
+    public static ArrayList<String> sourcephoneList = new ArrayList<>();
+    public static ArrayList<String> targetphoneList = new ArrayList<>();
+    public static ArrayList<String> questiontimeList = new ArrayList<>();
+    public static ArrayList<String> answertimeList = new ArrayList<>();
+//    public static ArrayList<Integer> idList;
+//    public static ArrayList<String> questionList;
+//    public static ArrayList<String> answerList;
+//    public static ArrayList<String> sourcephoneList;
+//    public static ArrayList<String> targetphoneList;
+//    public static ArrayList<String> questiontimeList;
+//    public static ArrayList<String> answertimeList ;
     public static int nowpos;   // 当前选中的问题在列表中的索引值
 
 }
